5cbabd18b50162c21938b2455b3c931c6d1ac483
Karthik Burra <kburra193@gmail.com>
2024-07-28 18:27:52 -0500

    slider component

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss'
 })
-export class HomeComponent {
+export class SliderComponent {
 ngOnInit(): void {
   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
   //Add 'implements OnInit' to the class.
-  console.log('HomeComponent initialized');
+  console.log('SliderComponent initialized');
 }
 }
--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './movie-list.component.scss'
 })
 export class MovieListComponent {
-
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  console.log('MovieListComponent initialized');
+}
 }
