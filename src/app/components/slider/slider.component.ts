2cc215c93e35895353d346d857325b5580e495e1
Karthik Burra <kburra193@gmail.com>
2024-07-29 16:21:43 -0500

    show data in frontend

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -7,17 +7,24 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './slider.component.scss',
 })
 export class SliderComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  movies: any;
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     console.log('SliderComponent initialized');
+    this.getPopularMovies();
+  }
+
+  getPopularMovies() {
     this.http
       .get(
         'https://api.themoviedb.org/3/movie/popular?api_key=f412fcdc374f38cc4d3df630ea8d20be',
       )
       .subscribe((data) => {
         console.log(data);
+        this.movies = data;
       });
   }
 }
