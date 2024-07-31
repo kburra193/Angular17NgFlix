a424e187b712abff4245bc2295f874d12bd05316
Karthik Burra <kburra193@gmail.com>
2024-07-31 16:16:47 -0500

    Define Movie Model

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MovieDto } from '../types/move';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
   getPopularMovies() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return this.http.get<any>(
+    return this.http.get<MovieDto>(
       'https://api.themoviedb.org/3/movie/popular?api_key=f412fcdc374f38cc4d3df630ea8d20be',
     );
   }
