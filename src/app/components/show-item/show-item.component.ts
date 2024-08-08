0e65765e36c254434b27ff060fc943e98d0e23ad
Karthik Burra <kburra193@gmail.com>
2024-08-08 15:52:53 -0500

    use input for component

--- src/app/components/show-item/show-item.component.ts
+++ src/app/components/show-item/show-item.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from './../../types/move';
+import { imagesBaseUrl } from './../../constants/images-size';
 
 @Component({
   selector: 'app-show-item',
   templateUrl: './show-item.component.html',
-  styleUrl: './show-item.component.scss'
+  styleUrl: './show-item.component.scss',
 })
 export class ShowItemComponent {
+  @Input() showItem: Movie | null = null;
 
+  imagesBaseUrl = imagesBaseUrl;
 }
