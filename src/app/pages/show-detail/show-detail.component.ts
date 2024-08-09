54d8ad5309d7f89a7559e75df61282dced3fcd42
Karthik Burra <kburra193@gmail.com>
2024-08-08 22:33:34 -0500

    get movie by id

--- src/app/pages/show-detail/show-detail.component.ts
+++ src/app/pages/show-detail/show-detail.component.ts
@@ -1,5 +1,8 @@
+import { MoviesService } from './../../services/movies.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Movie } from '../../types/movie';
 
 @Component({
   selector: 'app-show-detail',
@@ -7,13 +10,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './show-detail.component.scss',
 })
 export class ShowDetailComponent implements OnInit {
-  showId = 0;
-  constructor(private router: ActivatedRoute) {}
+  showId = '';
+  show$: Observable<Movie> | null = null;
+  constructor(
+    private router: ActivatedRoute,
+    private moviesService: MoviesService,
+  ) {}
   ngOnInit() {
     //observe the route params and subscribe to params changes
     this.router.params.subscribe((params) => {
       console.log(params);
       this.showId = params['id'];
+      this.show$ = this.moviesService.getMovieById(this.showId);
     });
     //another merthod instead of subscribing to params changes
     //this.router.snapshot.params['id'];
