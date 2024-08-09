38a357cffe209b69b98cdd4cee5c4161166865d8
Karthik Burra <kburra193@gmail.com>
2024-08-08 21:50:05 -0500

    reading route parameter value

--- src/app/pages/show-detail/show-detail.component.ts
+++ src/app/pages/show-detail/show-detail.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-show-detail',
   templateUrl: './show-detail.component.html',
   styleUrl: './show-detail.component.scss',
 })
-export class ShowDetailComponent {}
+export class ShowDetailComponent implements OnInit {
+  showId = 0;
+  constructor(private router: ActivatedRoute) {}
+  ngOnInit() {
+    //observe the route params and subscribe to params changes
+    this.router.params.subscribe((params) => {
+      console.log(params);
+      this.showId = params['id'];
+    });
+    //another merthod instead of subscribing to params changes
+    //this.router.snapshot.params['id'];
+  }
+}
